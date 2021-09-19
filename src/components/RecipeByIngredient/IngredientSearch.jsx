e61aaf765f348955d6566a5d7282983ae6733b60
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-09-19 14:20:26 -0700

    Created new useForm hook for ingredient search.

--- src/components/RecipeByIngredient/IngredientSearch.jsx
+++ src/components/RecipeByIngredient/IngredientSearch.jsx
@@ -1,44 +1,63 @@
 import React from "react";
-import useForm from "../Search/UseForm";
+import useForm from "./UseForm";
 import { Button } from "react-bootstrap";
+import axios from "axios";
 
 const IngredientSearch = () => {
     const { values, handleChange, handleSubmit } = useForm();
 
+    const makeIngredientRequest = async () => {
+        try {
+            let response = await axios.get(
+                `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${values}&number=2`
+            );
+            console.log(response.data);
+        } catch (ex) {}
+    };
+
+    const handleClick = () => {
+        makeIngredientRequest();
+    };
+
     return (
         <div>
+            <h3>Enter your pantry/fridge ingredients here!</h3>
             <form onSubmit={handleSubmit}>
                 <input
                     name="ingredientOne"
-                    value="{values}"
+                    value={values.ingredientOne}
                     placeholder="Ingredient One"
                     onChange={handleChange}
                 />{" "}
                 <input
                     name="ingredientTwo"
-                    value={values}
-                    placeholder="Ingredient One"
+                    value={values.ingredientTwo}
+                    placeholder="Ingredient Two"
                     onChange={handleChange}
                 />{" "}
                 <input
                     name="ingredientThree"
-                    value={values}
-                    placeholder="Ingredient One"
+                    value={values.ingredientThree}
+                    placeholder="Ingredient Three"
                     onChange={handleChange}
                 />{" "}
                 <input
                     name="ingredientFour"
-                    value={values}
-                    placeholder="Ingredient One"
+                    value={values.ingredientFour}
+                    placeholder="Ingredient Four"
                     onChange={handleChange}
                 />{" "}
                 <input
                     name="ingredientFive"
-                    value={values}
-                    placeholder="Ingredient One"
+                    value={values.ingredientFive}
+                    placeholder="Ingredient Five"
                     onChange={handleChange}
                 />{" "}
-                <Button type="submit" variant="secondary">
+                <Button
+                    type="submit"
+                    variant="secondary"
+                    onClick={() => handleClick()}
+                >
                     Search for Recipes!
                 </Button>{" "}
             </form>
