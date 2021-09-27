111cf3902e2e137dc013daa24b65c04b5b94473b
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-09-27 16:04:04 -0700

    Small text changes

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,7 +1,7 @@
 const Home = ({ user }) => {
     return (
         <div id="pages">
-            <h1>Welcome to Recipes Galore. Use the NavBar to get started!</h1>
+            <h1>Welcome to Recipe's Galore! Use the NavBar to get started.</h1>
             <img
                 id="img"
                 src="https://www.sheknows.com/wp-content/uploads/2018/08/gqcpoqotbd2xkqjb5x8n.jpeg"
