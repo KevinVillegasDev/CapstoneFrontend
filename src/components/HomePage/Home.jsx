45847cbc959bbcc4b7444419ca8ff43b65d7b454
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-10-15 10:13:03 -0700

    Tweaked css and added a new css file for home page.

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,9 +1,11 @@
+import "./Home.css";
+
 const Home = ({ user }) => {
     return (
-        <div id="pages">
-            <h1>Welcome to Recipe's Galore!</h1>
+        <div id="imagefront">
+            <h1 id="h1">Welcome to Recipe's Galore!</h1>
             <img
-                id="img"
+                id="center-fit"
                 src="https://www.sheknows.com/wp-content/uploads/2018/08/gqcpoqotbd2xkqjb5x8n.jpeg"
                 alt="cooking"
             />{" "}
